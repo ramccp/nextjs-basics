1d90fe6628cda5d28672cbc93c61eb035b0c0e29
Rampranav Vadlamani <rampranavccp@gmail.com>
2025-10-22 12:04:55 +0530

    2-server & client components

---
+++ src/app/components/Hello.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+function Hello(){
+    console.log("Hello, this is a client component.");
+
+    return <h1>Hello!</h1>
+}
+
+export default Hello;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
-import Image from "next/image";
+import Hello from "./components/Hello";
 
 export default function Home() {
+  console.log("Hello, this is rendered on server side.");
   return (
-    <h1 className="font-bold m-5 text-4xl">Welcome to CAT!</h1>
+    <>
+      <h1 className="font-bold m-5 text-4xl">Welcome to CAT!</h1>
+      <Hello />
+    </>
   );
 }
